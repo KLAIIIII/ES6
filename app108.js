57ec7d84a9f16a33d14fcebe1d5836427b92f4e4
KLAIIIII <guyyg03546@gmail.com>
2022-05-23 16:48:21 +0700

    Update

---
+++ app108.js
@@ -0,0 +1,13 @@
+//Assignment[=]
+let x = 20;
+// console.log(x)//20
+
+//Addittion Assignment[+=]
+let y = 20
+y += 10 //y = y + 10
+console.log(y)
+
+//Subtraction Assignment [-=]
+let z = 20
+z -= 10 //z = z - 10
+console.log(z)
