d968b134de62d327be8004eff71721260a6258b4
KLAIIIII <guyyg03546@gmail.com>
2022-05-23 16:31:45 +0700

    update

--- app107.js
+++ app107.js
@@ -1,6 +1,11 @@
 let x = 5;
 let y = 3;
 
+//increment
+console.log(`++x = ${++x}`)//6
+console.log(`x++ = ${x++}`)//return 6 then increment by 1
+console.log(`x = ${x}`)//6+1 = 7
+
 //remainder
 console.log(`x % y = ${x % y}`)
 
